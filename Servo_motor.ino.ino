4d6ecdeaba827811f74fdb5af07fa2816998c9cc
Mable Wan <mable-won@users.noreply.github.com>
2016-12-11 17:42:59 -0600

    Gripper program

---
+++ Servo_motor.ino.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+Servo servoMain; 
+
+void setup()
+{
+ servoMain.attach(10);
+ //Serial.begin(57600);
+  
+  }
+
+void loop(){
+  digitalWrite(2,HIGH);
+  closeServo();
+
+
+  }
+
+  void openServo(){
+    servoMain.write(90);
+    }
+
+      void closeServo(){
+    servoMain.write(30);
+    }
