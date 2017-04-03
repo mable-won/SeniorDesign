87dfe3f6f3a29a46006b7909dd2b5eeb9e65f923
redunn2 <fork416@yahoo.com>
2017-04-03 17:11:06 -0500

    SNGUI.fig + CRMain update
    added the figure for the sensor node gui and updated the CRMain to get
    voltages from the other coordinator.

--- Current Matlab Code/CRMain.m
+++ Current Matlab Code/CRMain.m
@@ -3,24 +3,38 @@
 % CRMain controls all charging robot functions, including assignment, 
 % tracking, alignment, attachment, and charging.
 %
-% version 0.0 by R. Dunn, M.C. Lalata, and M. Wan at the University of
-% Houston on 3/31/17
+% version 0.1 by R. Dunn, M.C. Lalata, and M. Wan at the University of
+% Houston on 4/3/17
 
 %% Global Variables
-global voltList; %#ok<NUSED>
-global outVector; %#ok<NUSED>
+global voltList;
+global outVector;
 global mov_package;
 global grip_package;
 global collision; %#ok<NUSED>
+%voltList = zeros(16);
+outVector = zeros(48);
 
 %% Initialization
-s = setupSerial('COM#');
-% get voltages
+s = setupSerial('COM7');
+
+%Use this for voltList until ready to coordinate with SNGUI on another
+%computer, then uncomment the code below, which will wait until SNGUI has
+%begun.
+voltList = [1 0 2 0 3 0 4 0 5 0 6 0 7 0 8 0];
+%[~,voltPackage]=receiveData(s,10); %voltPackage is array of targets ordered by voltage
+%for i=1:8 %voltList contains the array of sensor nodes and its assigned charging robot
+%    voltList(i*2-1)=voltPackage(i); 
+%end
+
+% Create timers (doesn't start them yet)
 t1 = createCRTimer(9);
 t2 = createCRTimer(10);
 t3 = createCRTimer(11);
 t4 = createCRTimer(12);
+
 % setup camera
+
 % Image processing
 CRTargetInit;
 
@@ -32,36 +46,36 @@ while (counter < 10) %to be replaced by get(hObject,'Value')
     for carID = 9:12
         switch carID %assign timer
             case 9
-                if (get(t1,'Running') == 'off')
+                if strcmp(get(t1,'Running'),'off')
                     [vLeft,vRight] = CRTracking(carID,t1);
                 else % charging
                     vLeft = 0;
                     vRight = 0; 
-                    grip_package(carID-8) = 0; %close gripper
+                    grip_package(carID-7) = 0; %close gripper
                 end
             case 10
-                if (get(t2,'Running') == 'off')
+                if strcmp(get(t2,'Running'),'off')
                     [vLeft,vRight] = CRTracking(carID,t2);
                 else % charging
                     vLeft = 0;
                     vRight = 0; 
-                    grip_package(carID-8) = 0; %close gripper
+                    grip_package(carID-7) = 0; %close gripper
                 end
             case 11
-                if (get(t3,'Running') == 'off')
+                if strcmp(get(t3,'Running'),'off')
                     [vLeft,vRight] = CRTracking(carID,t3);
                 else % charging
                     vLeft = 0;
                     vRight = 0; 
-                    grip_package(carID-8) = 0; %close gripper
+                    grip_package(carID-7) = 0; %close gripper
                 end
             case 12
-                if (get(t4,'Running') == 'off')
+                if strcmp(get(t4,'Running'),'off')
                     [vLeft,vRight] = CRTracking(carID,t4);
                 else % charging
                     vLeft = 0;
                     vRight = 0; 
-                    grip_package(carID-8) = 0; %close gripper
+                    grip_package(carID-7) = 0; %close gripper
                 end
         end
         mov_package((carID-8)*2) = vLeft;
@@ -70,7 +84,7 @@ while (counter < 10) %to be replaced by get(hObject,'Value')
     sendData(s,mov_package);
     sendData(s,grip_package);
     % image processing
-    counter = counter +1;
+    counter = counter + 1;
 end
 fclose(s);
 fdelete(s);
